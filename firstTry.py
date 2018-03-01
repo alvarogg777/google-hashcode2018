f465b82bd7373840c97e2ce179e6186b625b8aac
rusiano <rusiano@users.noreply.github.com>
2018-03-01 20:05:39 +0100

    Read all files

--- firstTry.py
+++ firstTry.py
@@ -1,23 +1,28 @@
 import numpy as np
 import pandas as pd
 
-f = open("b_should_be_easy.in", 'r')
-rides = f.read().split("\n")
-rides_df = pd.DataFrame([ride.split(" ") for ride in rides],
+allfiles = ['b_should_be_easy.in', 'c_no_hurry.in', 'd_metropolis.in', 'e_high_bonus.in']
+alldfs = []
+
+for file in allfiles:
+    f = open(file, 'r')
+    df = pd.DataFrame([ride.split(" ") for ride in f.readlines()],
                   dtype=float,
                   columns=['start_row', 'start_col', 'end_row', 'end_col', 'time_earliest', 'time_latest'])
+    alldfs.append(df)
 
-# remove last row
-rides_df = rides_df[:-1]
-rides_df['start_row'] = rides_df['start_row'].astype(float)
+b_df, c_df, d_df, e_df = alldfs
 
+"""
+LET'S WORK WITH THE FIRST DATASET: b_should_be_easy.in
+"""
 # sort by earliest time
-rides_df.sort_values(by='time_earliest', inplace=True)
+b_df.sort_values(by='time_earliest', inplace=True)
 
 # save useful variables
-NUM_ROWS, NUM_COLS, NUM_VEHICLES, NUM_RIDES, NUM_BONUS, NUM_STEPS = rides_df.loc[0, :]
+b_NUM_ROWS, b_NUM_COLS, b_NUM_VEHICLES, b_NUM_RIDES, b_NUM_BONUS, b_NUM_STEPS = b_df.loc[0, :]
 
 # remove first row
-rides_df = rides_df[1:]
+b_df = b_df[1:]
 
-print(rides_df.head())
+print(b_df.head())
